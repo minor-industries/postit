6d1e4082ec06b6103a8982f7067849a3813b4b44
suurkivi <136639517+suurkivi@users.noreply.github.com>
2024-06-26 13:46:55 -0700

    server running

---
+++ src/main.rs
@@ -0,0 +1,66 @@
+#[macro_use] extern crate rocket;
+use rocket::fs::{FileServer, NamedFile};
+use rocket::response::Redirect;
+use rocket::serde::json::Json;
+use rocket::serde::Serialize;
+use rocket::State;
+use std::path::PathBuf; // Import PathBuf here
+use std::net::SocketAddr;
+use structopt::StructOpt;
+use rust_embed::RustEmbed;
+
+#[derive(StructOpt, Debug)]
+#[structopt(name = "postit")]
+struct Opt {
+    #[structopt(long, env = "ADDR", default_value = "127.0.0.1:8000")]
+    addr: String,
+
+    #[structopt(long = "static-path")]
+    static_path: Option<String>,
+}
+
+#[derive(RustEmbed)]
+#[folder = "static/"]
+struct Asset;
+
+#[derive(Serialize)]
+struct Response {
+    message: &'static str,
+}
+
+#[get("/")]
+fn index() -> Redirect {
+    Redirect::temporary(uri!(postit))
+}
+
+#[get("/postit.html")]
+async fn postit(static_path: &State<Option<String>>) -> Option<NamedFile> {
+    if let Some(path) = &**static_path {
+        NamedFile::open(PathBuf::from(path).join("postit.html")).await.ok()
+    } else {
+        NamedFile::open(PathBuf::from("static/postit.html")).await.ok()
+    }
+}
+
+#[get("/json")]
+fn get_json() -> Json<Response> {
+    Json(Response {
+        message: "This is a constant JSON response.",
+    })
+}
+
+#[launch]
+fn rocket() -> _ {
+    let opt = Opt::from_args();
+
+    let addr: SocketAddr = opt.addr.parse().expect("invalid address");
+
+    rocket::custom(rocket::Config {
+        address: addr.ip(),
+        port: addr.port(),
+        ..rocket::Config::default()
+    })
+        .manage(opt.static_path.clone())
+        .mount("/", routes![index, postit, get_json])
+        .mount("/static", FileServer::from("static"))
+}
