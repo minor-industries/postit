602bde79b13dd06aed6cffaa6b884d61ea7375b6
suurkivi <136639517+suurkivi@users.noreply.github.com>
2024-06-27 14:06:26 -0700

    WIP: switch to hyper

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,19 @@
-#[macro_use] extern crate rocket;
-use rocket::fs::{FileServer, NamedFile};
-use rocket::response::Redirect;
-use rocket::serde::json::Json;
-use rocket::State;
-use std::net::SocketAddr;
-use std::path::PathBuf;
+use hyper::service::{make_service_fn, service_fn};
+use hyper::{Body, Request, Response, Server, StatusCode};
+use hyper::header::LOCATION;
+use hyper::http::Method;
+use serde_json::json;
+use sqlx::SqlitePool;
 use structopt::StructOpt;
 use rust_embed::RustEmbed;
-use sqlx::SqlitePool;
-use anyhow::Result;
+use std::convert::Infallible;
+use std::net::SocketAddr;
+use std::sync::Arc;
+use std::path::PathBuf;
+use tokio::fs::File;
+use tokio::io::AsyncReadExt;
 
 mod db;
-
 use crate::db::db::{init_db, load};
 
 #[derive(StructOpt, Debug)]
@@ -28,44 +30,148 @@ struct Opt {
 #[folder = "static/"]
 struct Asset;
 
-#[get("/")]
-fn index() -> Redirect {
-    Redirect::temporary(uri!(postit))
+async fn handle_index() -> Result<Response<Body>, Infallible> {
+    Ok(Response::builder()
+        .status(StatusCode::TEMPORARY_REDIRECT)
+        .header(LOCATION, "/postit.html")
+        .body(Body::empty())
+        .unwrap())
 }
 
-#[get("/postit.html")]
-async fn postit(static_path: &State<Option<String>>) -> Option<NamedFile> {
-    if let Some(path) = &**static_path {
-        NamedFile::open(PathBuf::from(path).join("postit.html")).await.ok()
+async fn handle_postit(static_path: Arc<Option<String>>) -> Result<Response<Body>, Infallible> {
+    let file_path = if let Some(ref path) = *static_path {
+        PathBuf::from(path).join("postit.html")
     } else {
-        NamedFile::open(PathBuf::from("static/postit.html")).await.ok()
+        PathBuf::from("static/postit.html")
+    };
+
+    match File::open(file_path).await {
+        Ok(mut file) => {
+            let mut contents = Vec::new();
+            file.read_to_end(&mut contents).await.unwrap();
+            Ok(Response::new(Body::from(contents)))
+        }
+        Err(_) => Ok(Response::builder()
+            .status(StatusCode::NOT_FOUND)
+            .body(Body::from("File not found"))
+            .unwrap()),
     }
 }
 
-#[post("/twirp/kv.KVService/LoadValue", data = "<key>")]
-async fn load_value(db: &State<SqlitePool>, key: String) -> Result<Json<Option<String>>, std::io::Error> {
-    let value = load(db.inner(), &key).await.map_err(|e| {
-        eprintln!("Failed to load value: {}", e);
-        std::io::Error::new(std::io::ErrorKind::Other, "Failed to load value")
-    })?;
-    Ok(Json(value))
+async fn handle_static_file(static_path: Arc<Option<String>>, req: Request<Body>) -> Result<Response<Body>, Infallible> {
+    let req_path = req.uri().path().trim_start_matches('/');
+
+    // Check if the requested path starts with "static"
+    let file_path = if let Some(ref path) = *static_path {
+        if req_path.starts_with("static/") {
+            PathBuf::from(path).join(req_path.trim_start_matches("static/"))
+        } else {
+            PathBuf::from(path).join(req_path)
+        }
+    } else {
+        if req_path.starts_with("static/") {
+            PathBuf::from("static").join(req_path.trim_start_matches("static/"))
+        } else {
+            PathBuf::from("static").join(req_path)
+        }
+    };
+
+    // Open the file and read its contents
+    match File::open(&file_path).await {
+        Ok(mut file) => {
+            let mut contents = Vec::new();
+            if let Err(e) = file.read_to_end(&mut contents).await {
+                eprintln!("Error reading file {}: {}", file_path.display(), e);
+                return Ok(Response::builder()
+                    .status(StatusCode::INTERNAL_SERVER_ERROR)
+                    .body(Body::from("Internal Server Error"))
+                    .unwrap());
+            }
+
+            // Determine the content type based on the file extension
+            let mime_type = match file_path.extension().and_then(std::ffi::OsStr::to_str) {
+                Some("html") => "text/html",
+                Some("css") => "text/css",
+                Some("js") => "application/javascript",
+                Some("png") => "image/png",
+                Some("jpg") | Some("jpeg") => "image/jpeg",
+                Some("gif") => "image/gif",
+                Some("svg") => "image/svg+xml",
+                Some("json") => "application/json",
+                _ => "application/octet-stream",
+            };
+
+            Ok(Response::builder()
+                .status(StatusCode::OK)
+                .header("Content-Type", mime_type)
+                .body(Body::from(contents))
+                .unwrap())
+        }
+        Err(_) => Ok(Response::builder()
+            .status(StatusCode::NOT_FOUND)
+            .body(Body::from("File not found"))
+            .unwrap()),
+    }
 }
 
-#[launch]
-fn rocket() -> _ {
-    let opt = Opt::from_args();
+async fn handle_load_value(db: Arc<SqlitePool>, req: Request<Body>) -> Result<Response<Body>, Infallible> {
+    let whole_body = hyper::body::to_bytes(req.into_body()).await.unwrap();
+    let key = String::from_utf8(whole_body.to_vec()).unwrap();
 
+    match load(&db, &key).await {
+        Ok(value) => {
+            let json_response = json!(value);
+            Ok(Response::new(Body::from(json_response.to_string())))
+        }
+        Err(e) => {
+            eprintln!("Failed to load value: {}", e);
+            Ok(Response::builder()
+                .status(StatusCode::INTERNAL_SERVER_ERROR)
+                .body(Body::from("Failed to load value"))
+                .unwrap())
+        }
+    }
+}
+
+async fn handle_request(
+    req: Request<Body>,
+    static_path: Arc<Option<String>>,
+    db: Arc<SqlitePool>,
+) -> Result<Response<Body>, Infallible> {
+    match (req.method(), req.uri().path()) {
+        (&Method::GET, "/") => handle_index().await,
+        (&Method::GET, "/postit.html") => handle_postit(static_path.clone()).await,
+        (&Method::POST, "/twirp/kv.KVService/LoadValue") => handle_load_value(db, req).await,
+        _ if req.uri().path().starts_with("/static/") => handle_static_file(static_path, req).await,
+        _ => Ok(Response::builder()
+            .status(StatusCode::NOT_FOUND)
+            .body(Body::from("Not Found"))
+            .unwrap()),
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    let opt = Opt::from_args();
     let addr: SocketAddr = opt.addr.parse().expect("invalid address");
 
-    let db = tokio::runtime::Runtime::new().unwrap().block_on(init_db("example.db")).unwrap();
+    let db = init_db("example.db").await.unwrap();
+    let db = Arc::new(db);
+    let static_path = Arc::new(opt.static_path.clone());
 
-    rocket::custom(rocket::Config {
-        address: addr.ip(),
-        port: addr.port(),
-        ..rocket::Config::default()
-    })
-        .manage(db)
-        .manage(opt.static_path.clone())
-        .mount("/", routes![index, postit, load_value])
-        .mount("/static", FileServer::from("static"))
+    let make_svc = make_service_fn(move |_| {
+        let static_path = static_path.clone();
+        let db = db.clone();
+        async {
+            Ok::<_, Infallible>(service_fn(move |req| {
+                handle_request(req, static_path.clone(), db.clone())
+            }))
+        }
+    });
+
+    let server = Server::bind(&addr).serve(make_svc);
+
+    if let Err(e) = server.await {
+        eprintln!("server error: {}", e);
+    }
 }
